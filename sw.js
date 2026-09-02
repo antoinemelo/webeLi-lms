4c861227729884dbec55f3af597d42ffb8899d36
WebeLi Release <noreply@lms.webe.li>
2026-09-02 08:26:00 +0200

    Publication liike 2026Q3.57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lms-shell-v100';
+const CACHE = 'lms-shell-v101';
 const SHELL = [
   'assets/vendor/bootstrap/bootstrap.min.css',
   'assets/vendor/bootstrap/bootstrap.bundle.min.js',
